500b3870077e1c0e86b418c403c1f82add27f625
AnhTuan <anhtuanpcipho@gmail.com>
2023-06-16 20:45:20 +0700

    init project

---
+++ displayInformation.py
@@ -0,0 +1,4 @@
+print('My name is Dao Anh Tuan')
+print("DOB: 14/10/1998")
+print("Address: Me Tri Thuong street, Nam Tu Liem district, Ha Noi capital")
+print("Hopefully the course designed by Codegym lectureres can help me so much on the road to become a Data Engineer")
